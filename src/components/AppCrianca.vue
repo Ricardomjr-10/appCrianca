73c8ff035d59f2a6f6106ed949f2297f51aa9e1e
Ricardo-10 <ricardomjr2@gmail.com>
2025-05-10 21:54:54 -0300

    create button para escolha do tema

--- src/components/AppCrianca.vue
+++ src/components/AppCrianca.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
         <h1>Escolha um tema</h1>
+        <div class="tema-escolha">
+            <button v-for="tema in temas" :key="tema.id" @click="selecionarTema(tema)">
+                {{ tema.nome }}
+            </button>
+        </div>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+    const temas = ref([])
+</script>
