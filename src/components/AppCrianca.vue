7060a3538652647e15f6dd10c76533bce84f0e1a
Ricardo-10 <ricardomjr2@gmail.com>
2025-05-10 22:12:11 -0300

    create div select item

--- src/components/AppCrianca.vue
+++ src/components/AppCrianca.vue
@@ -6,11 +6,23 @@
                 {{ tema.nome }}
             </button>
         </div>
+
+        <div v-if="temaSelecionado" class="cards-container">
+            <div v-for="item in itensDoTema" :key="item.id" class="card" @click="falarNome(item.nome)">
+                <img src="" alt="">
+                <p></p>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-    const temas = ref([])
+    const temas = ref([{id: 1, nome: 'Animais'}])
+
+    const selecionarTema = () => {
+        return alert('Tema selecionado: ' + temas.value[0].nome)
+    }
 </script>
