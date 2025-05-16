6b4ea0d5506921c7e74e08f78e628525c91fb1c0
Ricardo-10 <ricardomjr2@gmail.com>
2025-05-15 21:27:37 -0300

    treinando api

---
+++ src/components/ApiDog.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>particando API</h1>
+        <div class="img">
+            {{ urlImg }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+
+const urlImg = ref(null)
+
+
+
+</script>
